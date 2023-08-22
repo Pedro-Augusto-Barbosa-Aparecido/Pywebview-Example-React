165202e19b4453b5336c803217953b8bdd1ac43a
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2023-08-21 22:41:59 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import webview
+
+webview.create_window('TODO APPLICATION', 'index.html')
+webview.start(debug=False)
---
+++ scripts/main.jsx
@@ -0,0 +1,6 @@
+import React from "lib/react/development/react";
+import ReactDOM from "lib/react/development/react-dom";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <h1 className="text-white">lsnkcalskacnklasnclas</h1>
+)
