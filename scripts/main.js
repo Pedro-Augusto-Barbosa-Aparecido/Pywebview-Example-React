6215860d219917a2e3c5c5175f581815e7e8b6b0
Pedro Augusto Barbosa Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2023-08-22 08:20:22 -0300

    Feat: react code on html

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,3 @@
-import React from "lib/react/development/react";
-import ReactDOM from "lib/react/development/react-dom";
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <h1 className="text-white">lsnkcalskacnklasnclas</h1>
 )
